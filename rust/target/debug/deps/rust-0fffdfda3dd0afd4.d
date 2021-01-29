acb263f02da541042cc5865096bc24bbd69a171e
lxgr-linux <lxgr@protonmail.com>
2021-01-29 10:53:15 +0100

    added rust dirs

---
+++ rust/target/debug/deps/rust-0fffdfda3dd0afd4.d
@@ -0,0 +1,5 @@
+/home/arch/Python/scrap_engine/rust/target/debug/deps/rust-0fffdfda3dd0afd4: src/main.rs
+
+/home/arch/Python/scrap_engine/rust/target/debug/deps/rust-0fffdfda3dd0afd4.d: src/main.rs
+
+src/main.rs:
