1c55db106898ee591305e4195c8b8dd822ca459f
lxgr-linux <lxgr@protonmail.com>
2021-09-14 16:56:35 +0200

    Fixed typos

--- scrap_engine.py
+++ scrap_engine.py
@@ -2,11 +2,11 @@
 """
 Ascii game engine for the terminal.
 
-The main data scructures are Map and Object.
+The main data structures are Map and Object.
 Maps are objects, Object objects can be added to and then can be shown on
 the screen.
 
-ObjectGroup and their daughters can be used to automate generetaing, adding,
+ObjectGroup and their daughters can be used to automate generating, adding,
 removing etc. for a list of objects in their defined manner.
 
 States:
@@ -20,11 +20,11 @@ arg_proto:
     arg_proto is an dictionary that is given to an object by
     the programmer or an object_group(circle, frame, etc.) via the ob_args
     argument.
-    This can be used to store various extra values and is especially usefull
+    This can be used to store various extra values and is especially useful
     when using daughter classes of Object that needs extra values.
 
 This software is licensed under the GPL3
-You should have gotten an copy of the GPL3 license anlonside this software
+You should have gotten an copy of the GPL3 license alongside this software
 Feel free to contribute what ever you want to this engine
 You can contribute here: https://github.com/lxgr-linux/scrap_engine
 """
