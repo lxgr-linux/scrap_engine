3484759091542b9a7dece25e437e2dff75dd52ab
ttpf-P <65406575+ttpf-P@users.noreply.github.com>
2021-09-15 12:43:33 +0200

    Update scrap_bench.py

--- tests/scrap_bench.py
+++ tests/scrap_bench.py
@@ -4,31 +4,32 @@ import scrap_engine as se
 import time, random, os
 
 def main():
+    global avr, tcount
+
     os.system("")
-    map = se.Map(background=" ")
+    b_map = se.Map(background=" ")
     text = se.Text("0")
     avr = se.Text("0")
-    text.add(map, round((map.width-len(str(text.text)))/2-10), round(map.height/2)-1)
-    avr.add(map, round((map.width-len(str(text.text)))/2-10), round(map.height/2))
+    rectangle = se.Square('\033[34ma\033[0m', 50, 10)
+    text.add(b_map, round((b_map.width-len(text.text))/2-10),
+             round(b_map.height/2)-1)
+    avr.add(b_map, round((b_map.width-len(text.text))/2-10),
+            round(b_map.height/2))
+    rectangle.add(b_map, 0, 0)
+
     tcount = 0
     times = 0
-    obs = []
-    for i in range(50):
-        for j in range(10):
-            exec("ob_%s_%s = se.Object('\033[34ma\033[0m')"%(str(i), str(j)))
-            exec("ob_%s_%s.add(map, i, j)"%(str(i), str(j)))
-            exec("obs.append(ob_%s_%s)"%(str(i), str(j)))
-
-    map.show(init=True)
     time2 = 0
+
+    b_map.show()
     while True:
         time0 = time.time()
         times += time2
-        for ob in obs:
+        for ob in rectangle.obs:
             ob.set(random.choice([ob.x, ob.x+1, ob.x-1]), random.choice([ob.y, ob.y+1, ob.y-1]))
         text.rechar(str(time2))
         avr.rechar(str(times/tcount if tcount != 0 else 1))
-        map.show()
+        b_map.show()
         tcount += 1
         time2 = time.time()-time0
 
@@ -36,4 +37,8 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("KeyboardInterrupt")
+        print(f"""KeyboardInterrupt
+
+Screen size:        {se.screen_width}x{se.screen_height}
+Average frame time: {avr.text}s
+Frames drawn:       {tcount}""")
