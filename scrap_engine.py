3eb8c3021da07d36d68e635efec2d79301057f3d
lxgr-linux <lxgr@protonmail.com>
2021-12-19 19:38:25 +0100

    version naming

--- scrap_engine.py
+++ scrap_engine.py
@@ -30,7 +30,7 @@ You can contribute here: https://github.com/lxgr-linux/scrap_engine
 """
 
 __author__ = "lxgr <lxgr@protonmail.com>"
-__version__ = "0.3.3"
+__version__ = "1.1.0"
 
 import math
 import os
